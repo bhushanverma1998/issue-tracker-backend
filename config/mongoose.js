fbea01fb9c2c24c68caea842de438df4836f7adc
bhushanverma1998 <bhushanverma.verma@gmail.com>
2023-03-04 10:01:25 +0530

    env updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 //connect to db
-mongoose.connect('mongodb://localhost:27017/issue_tracker_db', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     family: 4,
